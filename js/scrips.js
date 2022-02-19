d534124e14e22f7f3723fc0b7b1fa21052cc7590
raphaelpacheco18 <raphaelsousa18@gmail.com>
2022-02-19 01:29:09 -0300

    DARK MODE

---
+++ js/scrips.js
@@ -0,0 +1,18 @@
+function changeMode(){
+    changeClasses();
+}
+
+function changeClasses () {
+    button.classList.toggle('dark-mode');
+    body.classList.toggle('dark-mode');
+    h1.classList.toggle('dark-mode');
+    footer.classList.toggle('dark-mode');
+}
+
+const button = document.getElementById('mode-selector');
+const h1 = document.getElementById('page-title');
+const body = document.getElementsByTagName('body')[0];
+const footer = document.getElementsByTagName('footer')[0];
+
+
+button.addEventListener('click', changeMode);
